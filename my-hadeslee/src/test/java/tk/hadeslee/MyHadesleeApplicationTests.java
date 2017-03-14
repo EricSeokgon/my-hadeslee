9775279109d432de43ef7d7e171aec8dc976ca36
sklee <hadesleeq@gmail.com>
2017-03-14 16:39:19 +0900

    Initial commit

---
+++ my-hadeslee/src/test/java/tk/hadeslee/MyHadesleeApplicationTests.java
@@ -0,0 +1,16 @@
+package tk.hadeslee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyHadesleeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
